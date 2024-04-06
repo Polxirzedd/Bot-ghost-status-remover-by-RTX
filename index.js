b56772b0bc30272f15ca82f48d4361a7dac1126e
Polxirzedd <166207582+Polxirzedd@users.noreply.github.com>
2024-04-06 18:50:58 +1100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Overviewing Polxrizedd's Bots"];
 
 
 let currentIndex = 0;
